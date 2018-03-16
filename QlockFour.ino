1acfc8e0598e0d908a84bb3dc3e0a94243a9f71f
Nosrob <contact.nosrob@gmail.com>
2018-03-16 22:00:08 +0100

    Initialize LED and clock stuff

---
+++ QlockFour.ino
@@ -0,0 +1,31 @@
+#include <DS3231.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+#define NUM_LEDS 160
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+DS3231 Clock;
+
+void setup() {
+
+  // Start the serial port
+  Serial.begin(57600);
+
+  // Start the I2C interface
+  Wire.begin();
+
+  // Clear LED strip
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+}
+
+void loop() {
+}
