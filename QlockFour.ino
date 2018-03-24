d74d409bd96d3a7ed4f176a4bcf79f5b340473d4
Nosrob <contact.nosrob@gmail.com>
2018-03-24 14:14:07 +0100

    Removed unused variable

--- QlockFour.ino
+++ QlockFour.ino
@@ -36,25 +36,6 @@ int pixelMap[ROWS][COLUMNS] = {
   {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 };
 
-byte neopix_gamma[] = {
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
-  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
-  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
-  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
-  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
-};
-
 void setup() {
   // Init serial port
   Serial.begin(9600);
@@ -78,7 +59,7 @@ void setup() {
   pinMode(BUT4, INPUT);
 
   // Init default mode
-  currentMode = MODE_DEFAULT;
+  currentMode = MODE_COLORCYCLE;
 }
 
 void loop() {
@@ -87,7 +68,6 @@ void loop() {
   if (digitalRead(BUT2) == HIGH) doButton2();
   if (digitalRead(BUT3) == HIGH) doButton3();
   if (digitalRead(BUT4) == HIGH) doButton4();
-
 
   if (currentMode == MODE_DEFAULT) {
     if (rtc.now().hour() == displayedTime.hour()
@@ -158,7 +138,6 @@ void doButton2() {
   delay(200);
 }
 
-
 // Add 1 hour
 void doButton3() {
   DateTime future (rtc.now() + TimeSpan(0, 1, 0, 0));
@@ -175,6 +154,7 @@ void showTime(uint32_t c) {
   int hour = rtc.now().hour();
   int minute = rtc.now().minute();
   int second = rtc.now().second();
+  displayedTime = rtc.now();
 
   IL(c);
   EST(c);
@@ -209,41 +189,6 @@ void showTime(uint32_t c) {
   if (hour == 1) HEURE(c);
   else if ((hour > 1 && hour < 12) || (hour > 12 && hour < 24)) HEURES(c);
 
-
-  Serial.print(hour, DEC);
-  Serial.print(":");
-  Serial.print(minute, DEC);
-  Serial.print(":");
-  Serial.println(second, DEC);
-
-  displayedTime = rtc.now();
   strip.show();
-}
-
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3, 0);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 0);
-}
-
-uint32_t dimmerWheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  float dimmerCoeff = 0.3;
-  if (WheelPos < 85) {
-    return strip.Color(dimmerCoeff * (255 - WheelPos * 3), 0, dimmerCoeff * (WheelPos * 3), 5);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, dimmerCoeff * (WheelPos * 3), dimmerCoeff * (255 - WheelPos * 3), 5);
-  }
-  WheelPos -= 170;
-  return strip.Color(dimmerCoeff * (WheelPos * 3), dimmerCoeff * (255 - WheelPos * 3), 0, 5);
 }
 
---
+++ Wheels.ino
@@ -0,0 +1,26 @@
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3, 0);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 0);
+}
+
+uint32_t dimmerWheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  float dimmerCoeff = 0.3;
+  if (WheelPos < 85) {
+    return strip.Color(dimmerCoeff * (255 - WheelPos * 3), 0, dimmerCoeff * (WheelPos * 3), 5);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, dimmerCoeff * (WheelPos * 3), dimmerCoeff * (255 - WheelPos * 3), 5);
+  }
+  WheelPos -= 170;
+  return strip.Color(dimmerCoeff * (WheelPos * 3), dimmerCoeff * (255 - WheelPos * 3), 0, 5);
+}
